26a6bc533ff436e8c5e90652699efc91a6db7364
itsoliviasparks <itsoliviasparks@gmail.com>
2024-07-04 17:02:44 -0600

    typo fix

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,13 +10,13 @@ const About = () => {
             </h2>
             <ul>
                 <li>
-                    I’m a curious & methodical developer, designer, and overall "keener" with 6+ years experience in collaborative product development
+                    I'm a curious & methodical developer, designer, and overall "keener" with 6+ years experience in collaborative project development
                 </li>
                 <li>
-                    I specialize in DHTML animation utilizing <Link to={externalLinks.gsap}>GSAP</Link> & Adobe Creative Suite; I craft succinct & maintainable code with a focus on performance
+                    I specialize in DHTML animation utilizing <Link to={externalLinks.gsap}>GSAP</Link>, Adobe Creative Suite, and Figma; I craft succinct & maintainable code with a focus on performance
                 </li>
                 <li>
-                    I’m a proven leader who implements best practices & standard operating procedures while fostering clear communication, strategic thinking, and a culture of continuous improvement
+                    I'm a proven leader who implements best practices & standard operating procedures while fostering clear communication, strategic thinking, and a culture of continuous improvement
                 </li>
                 <li>
                     I identify & offer forward-thinking solutions to problems while operating effectively through ambiguity, challenges, and transitions
